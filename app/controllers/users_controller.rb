bbe84a1c642547b1e4a0a975aafb2b9ea42d5e3d
pabloaredu <pablo.adu83@gmail.com>
2018-03-11 22:23:52 +0000

    Testing response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     # params.each do |key,value|
     #   Rails.logger.warn "Param #{key}: #{value}"
     # end
-    Rails.logger.info "This are the params: #{params.to_h}"
+    Rails.logger.info "This are the params: #{user_params}"
     # user = User.new (params.user.name, params.user.license_plate, params.user.password, params.user.password_confirmation, params.user.email)
     # if user.save
     #   session[:user_id] = user.id
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 
   private
 
-  # def user_params
-  #   params.require(:user).permit(:name, :license_plate, :password, :password_confirmation, :email)
-  # end
+  def user_params
+    params.require(:user).permit(:name, :license_plate, :password, :password_confirmation, :email)
+  end
 end
