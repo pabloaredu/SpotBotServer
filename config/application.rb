1225e1c5e427c91ce88c69a438eaf443a8c2a61c
pabloaredu <pablo.adu83@gmail.com>
2018-03-12 00:35:27 +0000

    Testing request

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module SpotbotServer
     config.load_defaults 5.1
     # config.web_console.whitelisted_ips = '10.0.2.2'
 
+    config.action_dispatch.default_headers.merge!('Cache-Control' => 'no-store, no-cache')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
