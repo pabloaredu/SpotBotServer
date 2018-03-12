15376cca682cabe4bcd0f19669d43d4829d2cf29
pabloaredu <pablo.adu83@gmail.com>
2018-03-12 04:05:07 +0000

    Testing request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,25 +8,24 @@ class UsersController < ApplicationController
     Rails.logger.debug params.inspect
 
     Rails.logger.info "This are the params: #{params}"
-    # @user = User.new (user_params)
-    # if @user.save
-    #   session[:user_id] = user.id
-    #   redirect_to '/'
-    # else
-    #   redirect_to '/signup'
-    # end
+    @user = User.new (user_params)
+    if @user.save
+      session[:user_id] = user.id
+      render json: {
+        status: 200,
+        message: "Hello there!",
+      }.to_json
+    else
+      # redirect_to '/signup'
+    end
 
     # Client.new(params[:client])
 
-    render json: {
-      status: 200,
-      message: "Hello there!",
-    }.to_json
   end
 
   private
 
-  # def user_params
-  #   params.require(:user).permit(:name, :license_plate, :password, :password_confirmation, :email)
-  # end
+  def user_params
+    params.require(:user).permit(:name, :license_plate, :password, :password_confirmation, :email)
+  end
 end
