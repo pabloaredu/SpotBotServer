46c6b66ac4729ab1f243e4cefd55d43008d61ead
PreranaShrestha <prerana.sh@gmail.com>
2018-03-17 03:57:32 +0000

    make resrevation controller work

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,15 +1,25 @@
 class ReservationsController < ApplicationController
 
   def create
-    spot = Spot.find_by(id: params[:spot_id])
 
-    reservation = spot.reservations.new(reservation_params)
-    reservation.user_id = 1
+  spot = Spot.find_by(id: params[:spot_id])
 
-    if reservation.save!
-      spot.update(availability: false)
-    render json: reservation
+  reservation = spot.reservations.new(reservation_params)
+  reservation.user_id = 1
+
+  if reservation.save!
+    spot.update(availability: false)
+    arr_pa_and_slots = []
+    data = Hash.new
+    @parking_areas = ParkingArea.all # get all the parking areas
+    @parking_areas.each do |parking_area|
+      @spots = parking_area.spots
+     data[:parking_area] = parking_area
+     data[:spots] = @spots
+     arr_pa_and_slots.push(data)
     end
+   render :json => {:parking_areas => arr_pa_and_slots}
+  end
 
   end
 
@@ -21,6 +31,5 @@ class ReservationsController < ApplicationController
     def reservation_params
       params.require(:reservation).permit(:reservation_status)
     end
-
 
 end
